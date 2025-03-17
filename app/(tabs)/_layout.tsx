19ba76057ccdc021bba272c39db03123592ca684
Adrian Burgos <aburgos@datamart.co>
2025-03-17 03:03:47 -0400

    add tabs navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,32 @@
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "blue",
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="map"
+        options={{
+          title: "Map",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons size={28} name="map" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
